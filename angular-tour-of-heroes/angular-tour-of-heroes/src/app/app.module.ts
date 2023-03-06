5cccea297aaf27f4155b7e9ba01b36c3b236a520
endrju <endrju@ncdc.pl>
2023-03-06 17:05:59 +0100

    added input for hero name

--- angular-tour-of-heroes/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
