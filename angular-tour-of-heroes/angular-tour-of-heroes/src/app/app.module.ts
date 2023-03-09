42dafd2af9536a152fa7dadef0f2e86791f8230d
endrju <endrju@ncdc.pl>
2023-03-09 10:56:43 +0100

    added services

--- angular-tour-of-heroes/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
     imports: [
         BrowserModule,
--- angular-tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from "./Hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,19 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  constructor(private heroService: HeroService) {}
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
